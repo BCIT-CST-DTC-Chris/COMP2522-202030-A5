8e148deef501f63ad48e1664a9d406a0a5f16150
CloverCode <sendmejunk4fun@hotmail.com>
2020-11-23 08:29:13 -0800

    Initial Commit - contains Dictionaries, Books, JUnit and stub classes.

---
+++ src/ca/bcit/comp2522/assignments/a5/Dictionary.java
@@ -0,0 +1,7 @@
+package ca.bcit.comp2522.assignments.a5;
+
+
+public class Dictionary {
+
+
+}
---
+++ src/ca/bcit/comp2522/assignments/a5/FileProcessorTest.java
@@ -0,0 +1,112 @@
+package ca.bcit.comp2522.assignments.a5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileProcessorTest {
+    //You may change this if the directory is incorrect
+    private static final String DIRECTORY = "A5/src/res/";
+
+    private static final int ENGLISH_WORDS = 370103;
+    private static final int FRENCH_WORDS = 426671;
+
+    private static final int SHERLOCK_LINES = 12310;
+    private static final int SHERLOCK_DISTINCT_WORDS = 7437;
+    private static final int SHERLOCK_TOTAL_WORDS = 95934;
+    private static final int SHERLOCK_NUM_THE = 5431;
+
+    private static final int EBOOK_LINES = 6831;
+    private static final int EBOOK_DISTINCT_WORDS = 5666;
+    private static final int EBOOK_TOTAL_WORDS = 44484;
+    private static final int EBOOK_NUM_UNE = 462;
+
+    FileProcessor fileProcessorEn;
+    FileProcessor fileProcessorFr;
+    Dictionary englishWords;
+    Dictionary frenchWords;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        fileProcessorEn = new FileProcessor(DIRECTORY+"SherlockHolmes.txt");
+        fileProcessorFr = new FileProcessor(DIRECTORY+"Une courte histoire de l'eBook.txt");
+        englishWords = new Dictionary(DIRECTORY+"dictionary_en.txt");
+        englishWords.load();
+        frenchWords = new Dictionary(DIRECTORY+"dictionary_fr.txt");
+        frenchWords.load();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        fileProcessorEn = null;
+        fileProcessorFr = null;
+        englishWords = null;
+        frenchWords = null;
+    }
+
+    @Test
+    public void testEnglishDictionarySize() {
+        assertEquals(englishWords.wordCount(), ENGLISH_WORDS);
+    }
+
+    @Test
+    public void testFrenchDictionarySize() {
+        assertEquals(frenchWords.wordCount(), FRENCH_WORDS);
+    }
+
+
+    @Test
+    public void testSherlockSize() {
+        assertEquals(fileProcessorEn.getNumLines(), SHERLOCK_LINES);
+    }
+
+    @Test
+    public void testSherlockDistinctWords() {
+        fileProcessorEn.calcWordCount(englishWords);
+        assertEquals(fileProcessorEn.distinctWordCount(), SHERLOCK_DISTINCT_WORDS);
+    }
+
+    @Test
+    public void testSherlockTotalWords() {
+        fileProcessorEn.calcWordCount(englishWords);
+        assertEquals(fileProcessorEn.totalWordCount(), SHERLOCK_TOTAL_WORDS);
+    }
+
+    @Test
+    public void testSherlockTheCount() {
+        fileProcessorEn.calcWordCount(englishWords);
+        assertEquals(fileProcessorEn.getNumOccurences("the"), SHERLOCK_NUM_THE);
+    }
+
+    @Test
+    public void testEBookSize() {
+        assertEquals(fileProcessorFr.getNumLines(), EBOOK_LINES);
+    }
+
+    @Test
+    public void testEBookDistinctWords() {
+        fileProcessorFr.calcWordCount(frenchWords);
+        assertEquals(fileProcessorFr.distinctWordCount(), EBOOK_DISTINCT_WORDS);
+    }
+
+    @Test
+    public void testEBookTotalWords() {
+        fileProcessorFr.calcWordCount(frenchWords);
+        assertEquals(fileProcessorFr.totalWordCount(), EBOOK_TOTAL_WORDS);
+    }
+
+    @Test
+    public void testEBookUneCount() {
+        fileProcessorFr.calcWordCount(frenchWords);
+        assertEquals(fileProcessorFr.getNumOccurences("une"), EBOOK_NUM_UNE);
+    }
+}
